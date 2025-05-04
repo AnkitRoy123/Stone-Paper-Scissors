beacd105d50ad152ef183f8519627540ca730704
AnkitRoy123 <166933200+AnkitRoy123@users.noreply.github.com>
2025-05-04 21:29:27 +0530

    main.py

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import random
-while True:
- x = ["Stone", "Paper", "Scissors"]
+while True:                                        x = ["Stone", "Paper", "Scissors"]
  x = random.choice(x)
  print("Choose any one :--")
  print("Stone, Paper and Scissors:")
  y = str(input(""))
  if x==y:
-    print("Draw")
- else:
+    print("Draw")                                  else:
     if (x == "Stone" and y == "Paper"):
-     print("Win")
-    elif (x == "Paper" and y == "Stone"):
+     print("Win")                                     elif (x == "Paper" and y == "Stone"):
      print("Lose")
     elif (x == "Stone" and y == "Scissors"):
      print("Lose")
     elif (x == "Scissors" and y == "Paper"):
-     print("Lose")
-    elif (y == "Stone" and x == "Scissors"):
+     print("Lose")                                    elif (y == "Stone" and x == "Scissors"):
      print("Win")
     elif (x == "Scissors" and y == "Paper"):
      print("Win")
     elif y == "exit":
      break
     else:
-     print("Choose any one into\nStone, Paper and Sci>
-     print("If you want to quit please enter exit.")
+     print("Choose any one into\nStone, Paper and>
+     print("If you want to quit please enter exit>
+ print(f"{y} vs {x}\n")
